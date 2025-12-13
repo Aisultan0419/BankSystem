d6fcf94dad57f3ba16ad6018b67bc9eb0e3e70d1
Aisultan0419 <kalebekaisultan@gmail.com>
2025-12-13 22:19:39 +0500

    Added tests

---
+++ src/Infrastructure/TransactionServices/EfExecutionStrategyRunner.cs
@@ -0,0 +1,28 @@
+﻿
+using Infrastructure.DbContext;
+using Microsoft.EntityFrameworkCore.Query.Internal;
+
+namespace ApplicationTests.TransactionServicesTests.TransactionTests
+{
+    public class EfExecutionStrategyRunner : IExecutionStrategyRunner
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public EfExecutionStrategyRunner(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public Task ExecuteAsync(Func<Task> operation)
+        {
+            var strategy = _appDbContext.Database.CreateExecutionStrategy();
+            return strategy.ExecuteAsync<object, object>(
+                null!,
+                async (db, state, ct) => { await operation(); return null!; },
+                null,
+                CancellationToken.None
+                );
+        }
+    }
+
+}
